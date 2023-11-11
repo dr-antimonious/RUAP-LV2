13a97af83a183977a61353f033a04bd87a963521
leotumbas <leotumbas@users.noreply.github.com>
2023-11-11 23:30:01 +0100

    Creating the Contact Manager API Controllers

---
+++ Begin/ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManager.Controllers
+{
+    public class ContactController : Controller
+    {
+        public string[] Get()
+        {
+            return new[]
+            {
+                "Hello",
+                "World"
+            };
+        }
+    }
+}
